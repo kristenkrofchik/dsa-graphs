64392daa349b6e6a61b500767a71411795c687e1
Kristen Krofchik <k.krofchik@gmail.com>
2021-09-08 18:07:45 -0500

    passing all tests w/ recursive dfs

--- graph.js
+++ graph.js
@@ -46,10 +46,75 @@ class Graph {
   }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  /*depthFirstSearch(start) {
+    let toVisitStack = [start];
+    let seen = new Set(toVisitStack);
+
+    while(toVisitStack.length > 0) {
+      let currNode = toVisitStack.pop();
+
+      for(let adjacent of currNode.adjacent) {
+        if(!seen.has(adjacent)) {
+          seen.add(adjacent);
+          toVisitStack.push(adjacent);
+        }
+      }
+    }
+    let seenVals = [];
+    for(let el of seen) {
+      seenVals.push(el.value);
+    }
+    return seenVals;
+  }*/
+
+  depthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+
+    function traverse(vertex) {
+      // base case
+      if (!vertex) {
+        return null;
+      }
+      // visit node
+      visited.add(vertex);
+      result.push(vertex.value);
+
+      // visit neighbors
+      vertex.adjacent.forEach(neighbor => {
+        if (!visited.has(neighbor)) {
+          return traverse(neighbor);
+        }
+      });
+    }
+
+    traverse(start);
+
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    let toVisitQueue = [start];
+    let seen = new Set(toVisitQueue);
+
+    while(toVisitQueue.length > 0) {
+      let currNode = toVisitQueue.shift();
+
+      for(let adjacent of currNode.adjacent) {
+        if(!seen.has(adjacent)) {
+          toVisitQueue.push(adjacent);
+          seen.add(adjacent);
+        }
+      }
+    }
+    let seenVals = [];
+    for(let el of seen) {
+      seenVals.push(el.value)
+    }
+
+    return seenVals;
+  }
 }
 
 module.exports = {Graph, Node}
