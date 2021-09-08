e643e19811b4c6ba88b15855ac7f8de69203cf4a
Kristen Krofchik <k.krofchik@gmail.com>
2021-09-08 17:16:58 -0500

    addVertices passing

--- graph.js
+++ graph.js
@@ -16,7 +16,11 @@ class Graph {
   }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for(let node of vertexArray) {
+      this.addVertex(node);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
   addEdge(v1, v2) {}
